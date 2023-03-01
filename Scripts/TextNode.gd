a895bd417c79670c1a587facf3ec03e578705d98
Bruskoo <93473401+Bruskoo@users.noreply.github.com>
2023-03-01 17:14:49 +0300

    Added set color and set type buttons to text node

--- Scripts/TextNode.gd
+++ Scripts/TextNode.gd
@@ -3,6 +3,10 @@ extends GraphNode
 @onready var text_edit = $MarginContainer/VBoxContainer/HBoxContainer3/TextEdit
 @onready var previos_size = text_edit.size
 
+@onready var color1_button = $MarginContainer/VBoxContainer/HBoxContainer4/Color1
+@onready var color2_button = $MarginContainer/VBoxContainer/HBoxContainer4/Color2
+@onready var color3_button = $MarginContainer/VBoxContainer/HBoxContainer4/Color3
+@onready var color4_button = $MarginContainer/VBoxContainer/HBoxContainer4/Color4
 
 func _ready():
 	pass
@@ -20,3 +24,20 @@ func _on_expand_toggled(button_pressed):
 func _on_close_request():
 	GlobalServiceVariables.node_close_request(self)
 	queue_free()
+
+
+func _on_color_1_pressed():
+	print(color1_button.get_theme_stylebox("normal").get_bg_color())
+	self.get_theme_stylebox("frame").bg_color = color1_button.get_theme_stylebox("normal").get_bg_color()
+
+
+func _on_color_2_pressed():
+	self.get_theme_stylebox("frame").bg_color = color2_button.get_theme_stylebox("normal").get_bg_color()
+
+
+func _on_color_3_pressed():
+	self.get_theme_stylebox("frame").bg_color = color3_button.get_theme_stylebox("normal").get_bg_color()
+
+
+func _on_color_4_pressed():
+	self.get_theme_stylebox("frame").bg_color = color4_button.get_theme_stylebox("normal").get_bg_color()
