19db5016201c207c7ce10d311cd629efd256c1cb
Bruskoo <93473401+Bruskoo@users.noreply.github.com>
2023-03-04 00:37:07 +0300

    Fixed spawning node on holding RBM button

--- Scripts/GraphEdit.gd
+++ Scripts/GraphEdit.gd
@@ -1,5 +1,15 @@
 extends GraphEdit
 
+var mouse_pressed = false
+
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			mouse_pressed = true
+		else:
+			mouse_pressed = false
+
 
 func _ready():
 	GlobalServiceVariables.connect("NODE_CLOSE_REQUEST", _on_node_close_request)
@@ -24,17 +34,18 @@ func _on_node_close_request(node):
 	var instance_name = node.get_name()
 	for n in node_connections:	
 		if instance_name == n["from"]:
-#			print("disconecting ", instance_name, "from ", n["from"])
+			print("disconecting ", instance_name, " from ", n["from"])
 			disconnect_node(instance_name, 0, n["to"], 0)
 		if instance_name == n["to"]:
-#			print("disconecting ", n["to"], "from ", instance_name)
+			print("disconecting ", n["to"], "from ", instance_name)
 			disconnect_node(n["from"], 0, instance_name, 0)
 
 
 func _on_gui_input(event):
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT) and mouse_pressed == true:
 		var main_node = load("res://Nodes/TextNode.tscn")
 		main_node = main_node.instantiate()
 		add_child(main_node)
 		main_node.add_to_group('graph_nodes')
 		spawn_node_on_cursor(main_node)
+		mouse_pressed = false
